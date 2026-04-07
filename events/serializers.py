d349250f90215529df69a928f7d2ae1a81333561
j1ere <jeremywambua106@gmail.com>
2026-04-07 20:03:52 +0300

    cloudinary issues files 12

--- events/serializers.py
+++ events/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from .models import UpcomingEvent, RegularEvent, CalendarFile
 
+from cloudinary.utils import cloudinary_url
 
 # class CalendarFileSerializer(serializers.ModelSerializer):
 #     file_url = serializers.SerializerMethodField()
@@ -26,9 +27,17 @@ class CalendarFileSerializer(serializers.ModelSerializer):
 
     def get_file_url(self, obj):
         if obj.file:
+            url, options = cloudinary_url(
+                obj.file.name,
+                resource_type="raw",
+                type="authenticated",  # required for raw files
+                sign_url=True,         # generates a signed URL
+                expires=600            # valid for 10 minutes
+            )
             request = self.context.get('request')
             if request:
-                return request.build_absolute_uri(obj.file.url)
+                return request.build_absolute_uri(url)
+            return url
             # Fallback: hardcode the backend base URL if no request context
             # return f"http://localhost:8000{obj.file.url}"
         return None
